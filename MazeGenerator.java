7d607b497c063b88cf9241bc2d44c4c51384e7c2
jamijyaaa <eshmurzaevajamilya@gmail.com>
2025-04-13 17:58:20 +0600

    обавила метод main с инициализацией массива

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -3,3 +3,5 @@ public class MazeGenerator {
     private static final int HEIGHT = 21;
     private static char[][] maze;
 }
+public static void main(String[] args) {
+    maze = new char[HEIGHT][WIDTH]; }
